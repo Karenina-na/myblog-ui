745a7313054955df8a904ec338bc91c1ebd8d367
Cycle-linux <Cycle-linux@qq.com>
2022-06-10 00:17:34 +0800

    更改linux开发环境

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
 
   //配置开发环境的代理
   devServer: {
-    port: 8080,
-    host: "localhost",
+    port: 55555,
+    host: "192.168.229.131",
     https: false,
     open: true,
     proxy: {
