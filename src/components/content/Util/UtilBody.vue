556ade23710c41c7dbeeeea39b4a8e9f37ae887f
Karenina-na <weizixiang0@outlook.com>
2022-06-29 09:41:06 +0800

    完成工具页面设计

---
+++ src/components/content/Util/UtilBody.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="box">
+        <div>
+            <div class="image">
+                <a-image :width="200" :height="200" src="https://www.rabbitmq.com/img/logo-rabbitmq.svg" />
+            </div>
+            <div class="introduce">
+                <div style="font-size:30px;text-align: center;"></div>
+                <div style="font-size:17px;text-align: center;padding-top:20px;"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UtilBody"
+}
+</script>
+
+<style scope>
+.box {
+    width: 850px;
+    padding: 10px 20px 10px;
+    line-height: 1.6;
+    float: left;
+
+    background-color: rgba(255, 255, 255, 0.705);
+    border: 1px solid;
+    border-color: rgba(0, 0, 0, 0.027);
+    border-radius: 30px;
+
+    box-shadow: 0px 0px 15px 3px rgba(0, 0, 0, 0.048);
+
+    transition-property: -webkit-box-shadow box-shadow;
+    transition-duration: 0.3s;
+    transition-timing-function: ease;
+}
+
+/*悬停阴影*/
+.box:hover {
+    -webkit-box-shadow: 0 0 20px 0px rgba(0, 0, 0, 0.35);
+    box-shadow: 0 0 20px 0px rgba(0, 0, 0, 0.35);
+}
+
+/**图片 */
+.image {
+    float: left;
+}
+
+/**介绍 */
+.introduce {
+    width: 580px;
+    float: right;
+    letter-spacing: 1.2px;
+    line-height: 1.2;
+    white-space: normal;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+}
+</style>
--- src/views/UtilPage.vue
+++ src/views/UtilPage.vue
@@ -2,17 +2,18 @@
   <perfect-scrollbar>
     <UpSelect id="UpSelect" class="Select" :AboutMe="AboutMe"> </UpSelect>
     <div class="body">
-      
-    </div>
+      <UtilBody class="util"></UtilBody>
+    </div>  
   </perfect-scrollbar>
 </template>
 
 <script>
 import UpSelect from "@/components/common/UpSelect.vue";
+import UtilBody from "@/components/content/Util/UtilBody.vue";
 import { SelectAboutMe } from "@/network/Select.js";
 
 export default {
-  components: { UpSelect },
+  components: { UpSelect,UtilBody },
   mounted() {this.GetAboutMe()},
   methods: {
     //查找必要数据
@@ -65,6 +66,18 @@ export default {
 <style>
 @import url("@/assets/css/AboutMy/UpSelect.css");
 
+/*滚动条样式*/
+.ps {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  height: 100%;
+}
+
+/*搜索框样式*/
 .Select {
   -webkit-user-select: none;
 
@@ -101,5 +114,22 @@ export default {
       both;
     animation: flip-in-hor-top 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
   }
+}
+
+/**主体样式 */
+.body{
+  position: absolute;
+  top: 100px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: 850px;
+  z-index: 100;
+}
+
+/**工具栏样式 */
+.util{
+  margin-bottom: 20px;
 }
 </style>
